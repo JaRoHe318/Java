cec65327d70e5f59fc4f772b4c84e56396226df0
JaRoHe318 <jarohe1000@gmail.com>
2017-10-19 00:13:49 -0700

    Some mac work

--- Projects/Sand/src/Main.java
+++ Projects/Sand/src/Main.java
@@ -1,5 +1,6 @@
 
 import static java.lang.System.*;
+
 import java.util.*;
 import java.io.*;
 
@@ -7,30 +8,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner kb = new Scanner(in);
 
-        out.print("Enter a number: ");
-        int num = kb.nextInt();
-        int factors = 0;
-        int h =0;
+        String[] names = new String[]{"Jason", "Jensine", "Justine"};
 
-
-        for (int i = 1; i<=num ; ++i){
-            h = num%i;
-            System.out.println(num+"%"+i+" = "+h);
-            if(num%i==0){
-                factors++;
-                System.out.println("\n\nfactors:"+factors);
-            }
-
+        for (String name : names) {
+            System.out.println(name);
         }
-
-        if (factors==2){
-            out.println("Prime!");
-        }else{
-            out.println("Not P");
-        }
-
 
 
     }
