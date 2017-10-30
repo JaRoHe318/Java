17e4c809117d82ea34c08d86e7c1d6fedcbf0b96
JaRoHe318 <jarohe1000@gmail.com>
2017-10-30 13:24:26 -0700

    sand

--- Projects/Sand/src/Main.java
+++ Projects/Sand/src/Main.java
@@ -1,13 +1,28 @@
 
 import java.io.*;
 
-
-
 public class Main {
-    public static void main(String[] args) {
 
-        Baby test = new Baby();
+    static String reverse(String s){
+        String reversed;
+        //the reverse of string abcd is the reverse of this string d(abc)
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        int ans = fact(3);
 
 
     }
+
+    public int fact(int num){
+
+        if(num==0){
+            return 1;
+        }
+        int factOfRest = fact(num-1);
+        return num*factOfRest;
+    }
+
 }//class Main
